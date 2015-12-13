ef95830ccfa988e2e1906ace2d9515793062a41d
mhamilton723 <mark.hamilton@yale.edu>
2015-12-12 21:04:56 -0500

    Added License
    added license

--- strategy_test.py
+++ strategy_test.py
@@ -6,10 +6,10 @@ from utils import backtest
 #strategies = [MomentumStrategy, BuyAndHoldStrategy]
 #strategy_test(strategies, tickers)
 tickers = ['AAPL', 'QQQ', 'KMI', 'VZ', 'DD', 'VOD', 'CTL']
-#ms = MomentumStrategy(10000, tickers)
-#bs = BuyAndHoldStrategy(10000, tickers)
+ms = MomentumStrategy(10000, tickers)
+bs = BuyAndHoldStrategy(10000, tickers)
 ts = TSEBuyAndHoldStrategy(10000, tickers)
-#print(backtest(ms, correct=False))
-#print(backtest(bs, correct=False))
+print(backtest(ms, correct=False))
+print(backtest(bs, correct=False))
 print(backtest(ts, correct=False))
 
